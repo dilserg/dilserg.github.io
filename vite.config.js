7e883e1cd6a0052b6a2acad573b3977d1ef36215
Davydov Ilia Sergeevich <ildavydov@ozon.ru>
2024-05-01 20:45:59 +0300

    added all pages to build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: './index.html',
+        list: './list.html',
+        new: './new.html',
+      },
+    },
+  },
   define: {
     global: {},
   },
