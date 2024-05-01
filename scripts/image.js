912fc73ecaa6f9df9ecf79bfbf96856f35a08b02
Davydov Ilia Sergeevich <ildavydov@ozon.ru>
2024-05-01 21:19:34 +0300

    moved api key to code

--- scripts/image.js
+++ scripts/image.js
@@ -1,6 +1,6 @@
 import { uploadFile } from '@uploadcare/upload-client';
 
-const API_KEY = import.meta.env;
+const API_KEY = '388af66ef83dffd3b94a';
 
 const progress = document.getElementById('progress-bar');
 
@@ -12,16 +12,14 @@ const uploadPhoto = async (file) => {
   if (file.size > 1024 * 1024 * 10) {
     throw new Error('Слишком большой размер файла. Максимально - 10МБ');
   }
-  console.log(API_KEY);
-  // progress.classList.remove('hidden');
-  // const result = await uploadFile(file, {
-  //   publicKey: API_KEY,
-  //   store: 'auto',
-  //   onProgress: onUploadProgress,
-  // });
-  // progress.classList.add('hidden');
-  // return result.cdnUrl;
-  return 'https://ucarecdn.com/a67b9d7a-9a34-4984-a133-88158e08fd8f/20240302014251.jpg';
+  progress.classList.remove('hidden');
+  const result = await uploadFile(file, {
+    publicKey: API_KEY,
+    store: 'auto',
+    onProgress: onUploadProgress,
+  });
+  progress.classList.add('hidden');
+  return result.cdnUrl;
 };
 
 export { uploadPhoto };
